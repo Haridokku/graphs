a9244f649df35a41661bbaa100d7cdeb3345eea3
Haridokku <124425442+Haridokku@users.noreply.github.com>
2023-05-18 16:00:55 +0530

    Update index.js

--- src/components/VaccinationByGender/index.js
+++ src/components/VaccinationByGender/index.js
@@ -1,4 +1,4 @@
-import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
+import {PieChart, Pie, Legend, Cell} from 'recharts'
 import './index.css'
 
 const VaccinationByGender = props => {
@@ -6,30 +6,28 @@ const VaccinationByGender = props => {
   return (
     <>
       <h1 className="head">Vaccination by gender</h1>
-      <ResponsiveContainer width="100%" height={300}>
-        <PieChart>
-          <Pie
-            cx="70%"
-            cy="40%"
-            data={pieChart}
-            startAngle={0}
-            endAngle={180}
-            innerRadius="40%"
-            outerRadius="70%"
-            dataKey="count"
-          >
-            <Cell name="Male" fill="#f54394" />
-            <Cell name="Female" fill="#5a8dee" />
-            <Cell name="Others" fill="#2cc6c6" />
-          </Pie>
-          <Legend
-            iconType="circle"
-            layout="vertical"
-            verticalAlign="middle"
-            align="right"
-          />
-        </PieChart>
-      </ResponsiveContainer>
+      <PieChart width={1000} height={300}>
+        <Pie
+          cx="70%"
+          cy="40%"
+          data={pieChart}
+          startAngle={0}
+          endAngle={180}
+          innerRadius="40%"
+          outerRadius="70%"
+          dataKey="count"
+        >
+          <Cell name="Male" fill="#f54394" />
+          <Cell name="Female" fill="#5a8dee" />
+          <Cell name="Others" fill="#2cc6c6" />
+        </Pie>
+        <Legend
+          iconType="circle"
+          layout="vertical"
+          verticalAlign="middle"
+          align="right"
+        />
+      </PieChart>
     </>
   )
 }
