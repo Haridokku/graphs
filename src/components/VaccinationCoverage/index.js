8f1dee904dede73f5db565046d648f5a85367126
Haridokku <124425442+Haridokku@users.noreply.github.com>
2023-05-18 16:01:35 +0530

    Update index.js

--- src/components/VaccinationCoverage/index.js
+++ src/components/VaccinationCoverage/index.js
@@ -1,11 +1,4 @@
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  Legend,
-  ResponsiveContainer,
-} from 'recharts'
+import {BarChart, Bar, XAxis, YAxis, Legend} from 'recharts'
 
 import './index.css'
 
@@ -20,21 +13,16 @@ const VaccinationCoverage = props => {
   return (
     <>
       <h1 className="head">Vaccination Coverage</h1>
-      <ResponsiveContainer width="100%" height={500}>
-        <BarChart data={BarData} margin={{top: 5}}>
-          <XAxis
-            dataKey="vaccine_date"
-            tick={{stroke: 'gray', strokeWidth: 1}}
-          />
-          <YAxis
-            tickFormatter={DataFormatter}
-            tick={{stroke: 'gray', strokeWidth: 0}}
-          />
-          <Legend wrappedStyle={{padding: 30}} />
-          <Bar dataKey="dose1" name="Dose1" fill="#2d87bb" barSize="20%" />
-          <Bar dataKey="dose2" name="Dose2" fill=" #f54394" barSize="20%" />
-        </BarChart>
-      </ResponsiveContainer>
+      <BarChart width={1000} height={300} data={BarData} margin={{top: 5}}>
+        <XAxis dataKey="vaccine_date" tick={{stroke: 'gray', strokeWidth: 1}} />
+        <YAxis
+          tickFormatter={DataFormatter}
+          tick={{stroke: 'gray', strokeWidth: 0}}
+        />
+        <Legend wrappedStyle={{padding: 30}} />
+        <Bar dataKey="dose1" name="Dose1" fill="#2d87bb" barSize="20%" />
+        <Bar dataKey="dose2" name="Dose2" fill=" #f54394" barSize="20%" />
+      </BarChart>
     </>
   )
 }
